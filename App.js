0bce7cc492d83c5aba729474678130ed22d92684
LuseBiswas <lusebiswas@gmail.com>
2024-04-07 11:28:04 +0530

    First Push

--- App.js
+++ App.js
@@ -3,18 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-[40px] text-red-400 bg-green-400">Hello Ritesh</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: 'yellow',
+//    alignItems: 'center',
+//    justifyContent: 'center',
+//  },
+//});
+
+
