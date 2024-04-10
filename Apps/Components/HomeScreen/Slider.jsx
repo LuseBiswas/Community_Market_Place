d19b00b983f0b3a53b78d31b790bb110c7c3e771
LuseBiswas <lusebiswas@gmail.com>
2024-04-10 21:57:38 +0530

    So the automatic scroll Slider is made

--- Apps/Components/HomeScreen/Slider.jsx
+++ Apps/Components/HomeScreen/Slider.jsx
@@ -1,23 +1,65 @@
-import React, { Component } from 'react'
-import { FlatList, Image, Text, View } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react';
+import { FlatList, Image, View, TouchableOpacity } from 'react-native';
 
-export default function Slider({sliderList}){
-    
-  
-    return (
-      <View className='mt-5'>
-       <FlatList
-       data={sliderList}
-       horizontal={true}
-       showsHorizontalScrollIndicator={false}
-       renderItem={({item,index})=>(
-        <View>
-            <Image source={{uri:item?.image}}
-            className='h-[200px] w-[325px] mr-3 rounded-lg object-contain'/>
-        </View>
-       )}
-       />
-        
+export default function Slider({ sliderList }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const flatListRef = useRef(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let nextIndex = activeIndex + 1;
+      if (nextIndex === sliderList.length) {
+        nextIndex = 0;
+      }//If slider move to end then redirect to the first image
+      setActiveIndex(nextIndex);
+      flatListRef.current.scrollToIndex({ animated: true, index: nextIndex });
+    }, 3000); // Change slide every 3 seconds (adjust as needed)
+
+    return () => clearInterval(interval);
+  }, [activeIndex]);
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Image
+        source={{ uri: item?.image }}
+        style={{ height: 200, width: 325, marginRight: 3, borderRadius: 10 }}
+      />
+    </View>
+  );
+
+  const renderDot = (index) => (
+    <TouchableOpacity
+      key={index}
+      onPress={() => setActiveIndex(index)}
+      style={{
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: activeIndex === index ? '#007dfe' : 'gray',
+        marginHorizontal: 5,
+      }}
+    />
+  );
+
+  return (
+    <View style={{ marginTop: 5 }}>
+      <FlatList
+        ref={flatListRef}
+        data={sliderList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={renderItem}
+        pagingEnabled
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          marginTop: 10,
+        }}>
+        {sliderList.map((_, index) => renderDot(index))}
       </View>
-    )
+    </View>
+  );
 }
